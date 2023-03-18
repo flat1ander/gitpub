c9711e9b688a943f849061065b3b134c35e1f8eb
flat1ander <thematthewlane@gmail.com>
2023-03-18 15:45:09 -0400

    Setup index view

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const models = require('./models/drinks')
+const drinks = models.drinks
+
+app.get('/', (req, res) => {
+    res.send("Welcome to the GitPub App")
+})
+
+app.get('/drinks', (req, res) => {
+    res.render('drinks_index.ejs', {drinks})
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen('3000', () => {
+    console.log('Listening on port 3000')
+})
